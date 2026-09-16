425ea25ec1b867f3760b9e034d4eb47f5bd378c6
maria eugenia <meugeniamm30@gmail.com>
2026-09-16 17:56:31 -0300

    implementa pagina de proximos jogos

--- ingressos/views.py
+++ ingressos/views.py
@@ -3,4 +3,7 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'ingressos/home.html')
 
+def jogos(request):
+    return render(request, 'ingressos/jogos.html')
+
 # Create your views here.
